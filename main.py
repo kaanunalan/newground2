9ebf61f5c76d97ca37804d9a4569e5f933bec3ee
Kaan Unalan <e11907099@student.tuwien.ac.at>
2022-05-07 14:01:27 +0200

    Refactor main.py style

--- main.py
+++ main.py
@@ -7,15 +7,18 @@ from clingo_app import ClingoApp
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(prog='newground', usage='%(prog)s [files]')
-    parser.add_argument('--no-show', action='store_true', help='Do not print #show-statements to avoid compatibility issues. ')
+    parser.add_argument('--no-show', action='store_true',
+                        help='Do not print #show-statements to avoid compatibility issues. ')
     parser.add_argument('--ground-guess', action='store_true',
                         help='Additionally ground guesses which results in (fully) grounded output. ')
     parser.add_argument('--ground', action='store_true',
                         help='Output program fully grounded. ')
     parser.add_argument('file', type=argparse.FileType('r'), nargs='+')
     args = parser.parse_args()
+
     # no output from clingo itself
     sys.argv.append("--outf=3")
+
     no_show = False
     ground_guess = False
     ground = False
