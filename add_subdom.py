037e6ec7a5af1a0e9007404a8da5a7969aeec92a
Kaan Unalan <e11907099@student.tuwien.ac.at>
2022-05-08 09:50:45 +0200

    Add documentation for add_to_subdom in add_subdom.py

--- add_subdom.py
+++ add_subdom.py
@@ -1,4 +1,11 @@
 def add_to_subdom(subdoms, var, value):
+    """
+    Add the domains for variables and corresponding list of atoms to the dictionary of subdomains
+
+    :param subdoms: a dictionary of subdomains
+    :param var: a variable
+    :param value: a symbolic or numeric constant
+    """
     if var.startswith("_dom_"):
         var = var[5:]
     else:
