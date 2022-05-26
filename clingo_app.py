6b82929798045d9e819b86b45b482708cb819fa6
Kaan Unalan <e11907099@student.tuwien.ac.at>
2022-05-26 11:59:13 +0200

    Refactor clingo_app.py more

--- clingo_app.py
+++ clingo_app.py
@@ -17,22 +17,44 @@ class ClingoApp(Application):
         self.__no_show = no_show
         self.__ground_guess = ground_guess
         self.__ground = ground
-        # ground program representation
-        self.__prg = Program()
 
     def main(self, ctl, files):
+        # ground program representation
+        prg = Program()
         # Control object for grounding and solving
         ctl_insts = Control()
         # Register the observer to build a ground program representation while grounding
-        ctl_insts.register_observer(ProgramObserver(self.__prg))
+        ctl_insts.register_observer(ProgramObserver(prg))
 
         # read subdomains in #program insts.
         self.__read_subdoms(ctl_insts, files)
 
         if self.__ground:
-            print(self.__prg)
+            print(prg)
 
-        # Initialize term transformer
+        self.__transform_nglp_dlp(ctl, files)
+
+    def __read_subdoms(self, ctl_insts, files):
+        for f in files:
+            # Extend the logic program with a (non-ground) logic program in a file.
+            ctl_insts.load(f)
+
+        # Ground the program parts after #program insts (and other parts).
+        # ctl_insts.ground([("insts", [])]) # Why not?
+        ctl_insts.ground([("base", []), ("insts", [])])
+
+        for k in ctl_insts.symbolic_atoms:
+            if str(k.symbol).startswith("_dom_"):
+                # Get "_dom_X"
+                var = str(k.symbol).split("(", 1)[0]
+                # Replace all patterns of arbitrary characters up to the first opening parenthesis in strings
+                # with an empty string and remove also the last character: e.g. _dom_X(1) -> 1
+                atom = re.sub(r"^.*?\(", "", str(k.symbol))[:-1]
+                # Add the domains for variables and corresponding list of atoms to the dictionary of subdomains
+                add_to_subdom(self.__subdoms, var, atom)
+
+    def __transform_nglp_dlp(self, ctl, files):
+        # Initialize the term transformer
         term_transformer = TermTransformer(self.__subdoms, self.__no_show)
         # Parse the programs in the given files and return an abstract syntax tree for each statement via a callback
         # TODO: Is it necessary?
@@ -50,6 +72,7 @@ class ClingoApp(Application):
                 print(":- not sat.")
                 # parse_string(f"sat :- {','.join([f'sat_r{i}' for i in range(1, transformer.counter+1)])}.",
                 # lambda stm: self.bld.add(stm))
+
                 # Prints rule (6)
                 print(f"sat :- {','.join([f'sat_r{i}' for i in range(1, transformer.counter + 1)])}.")
 
@@ -70,27 +93,18 @@ class ClingoApp(Application):
                             head = ','.join(c)
                             print(f":- {', '.join([f'{p}' + (f'({head})' if len(head) > 0 else '')] + rule_sets)}.")
 
-                if not self.__ground_guess:
-                    for t in transformer.terms:
-                        print(f"dom({t}).")
+                self.__handle_ground_guess(transformer)
 
-                if not self.__no_show:
-                    if not term_transformer.show:
-                        for f in transformer.shows.keys():
-                            for l in transformer.shows[f]:
-                                print(f"#show {f}/{l}.")
+                self.__handle_no_show(transformer, term_transformer)
 
-    def __read_subdoms(self, ctl_insts, files):
-        for f in files:
-            # Extend the logic program with a (non-ground) logic program in a file.
-            ctl_insts.load(f)
+    def __handle_ground_guess(self, nglp_dlp_transformer):
+        if not self.__ground_guess:
+            for t in nglp_dlp_transformer.terms:
+                print(f"dom({t}).")
 
-        # Ground the program parts after #program insts.
-        ctl_insts.ground([("base", []), ("insts", [])])
-
-        for k in ctl_insts.symbolic_atoms:
-            if str(k.symbol).startswith('_dom_'):
-                var = str(k.symbol).split("(", 1)[0]
-                atom = re.sub(r'^.*?\(', '', str(k.symbol))[:-1]
-                # add the domains for variables and corresponding list of atoms to the dictionary of subdomains
-                add_to_subdom(self.__subdoms, var, atom)
+    def __handle_no_show(self, nglp_dlp_transformer, term_transformer):
+        if not self.__no_show:
+            if not term_transformer.show:
+                for f in nglp_dlp_transformer.shows.keys():
+                    for l in nglp_dlp_transformer.shows[f]:
+                        print(f"#show {f}/{l}.")
