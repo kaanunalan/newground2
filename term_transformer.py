6c5dfb7dade4e2de807e64e693c7d8cd31e170cd
Kaan Unalan <e11907099@student.tuwien.ac.at>
2022-05-27 08:35:21 +0200

    Make getters private and add documentation

--- term_transformer.py
+++ term_transformer.py
@@ -1,3 +1,7 @@
+"""
+This module collects some information about the program for further processing.
+"""
+
 import re
 
 from clingo.ast import Transformer
@@ -7,19 +11,24 @@ from add_subdom import add_to_subdom
 
 class TermTransformer(Transformer):
     def __init__(self, subdoms, no_show=False):
-        self.__terms = []
-        self.__subdoms = subdoms
-        self.__facts = {}
-        self.__ng_heads = {}
-        self.__ng = False
+        self.__terms = [] # Terms occuring in the program
+        self.__subdoms = subdoms # Subdomains
+        self.__facts = {} # Facts, arities and arguments
+        self.__ng_heads = {} # Rule heads with their arities
+        self.__ng = False # If the program is non-ground
         self.__show = False
-        self.__shows = {}
-        self.__current_f = None
+        self.__shows = {} # Predicates and their arities
+        self.__current_f = None # Current function (or predicate)
         self.__no_show = no_show
 
     def visit_Rule(self, node):
-        # Visits and transforms the children of the given AST
-        # Necessary for programs without instantiated domains
+        """
+        Visits rules of the program and saves non-ground head predicates with their arities
+        or saves facts with their arities and arguments.
+
+        :param node: Rule in the program.
+        :return Node of the AST.
+        """
         self.visit_children(node)
         # Get rule head predicate name
         pred = str(node.head).split("(", 1)[0]
@@ -50,6 +59,12 @@ class TermTransformer(Transformer):
         return node
 
     def visit_Function(self, node):
+        """
+        Visits functions (or predicates) of the program and saves their name and arity
+
+        :param node: Function node of the program.
+        :return: Node of the AST.
+        """
         self.__current_f = str(node).split("(", 1)[0] if len(node.arguments) > 0 else node
         # shows
         # if not str(node.name).startswith('_dom_'):
@@ -57,16 +72,26 @@ class TermTransformer(Transformer):
             self.__shows[node.name].add(len(node.arguments))
         else:
             self.__shows[node.name] = {len(node.arguments)}
-        # Visits and transforms the children of the given AST
-        # Necessary for programs without instantiated domains
         self.visit_children(node)
         return node
 
     def visit_Variable(self, node):
+        """
+        Visits variables of the program in order to determine if the program is non-ground.
+
+        :param node: Variable in the program.
+        :return Node of the AST.
+        """
         self.__ng = True
         return node
 
     def visit_Interval(self, node):
+        """
+        Visits intervals of the program in order to add them to subdomains.
+
+        :param node: Interval in the program.
+        :return Node of the AST.
+        """
         for i in range(int(str(node.left)), int(str(node.right))+1):
             if str(i) not in self.__terms:
                 self.__terms.append(str(i))
@@ -74,40 +99,52 @@ class TermTransformer(Transformer):
         return node
 
     def visit_SymbolicTerm(self, node):
+        """
+        Visits symbolic terms in order to add them to subdomains.
+
+        :param node: Symbolic term in the program.
+        :return: Node of the AST.
+        """
         if str(node) not in self.__terms:
             self.__terms.append(str(node))
         add_to_subdom(self.__subdoms, self.__current_f, str(node))
         return node
 
     def visit_ShowSignature(self, node):
-        self.__show = True
+        """
+        Visits the show directives in the program in order to print them (if the
+        --no-show option is not used)
+
+        :param node: Show directive in the program.
+        :return: Node of the AST.
+        """
         if not self.__no_show:
             print(node)
         return node
 
-    def get_terms(self):
+    def __get_terms(self):
         return self.__terms
 
-    def get_subdoms(self):
+    def __get_subdoms(self):
         return self.__subdoms
 
-    def get_facts(self):
+    def __get_facts(self):
         return self.__facts
 
-    def get_ng_heads(self):
+    def __get_ng_heads(self):
         return self.__ng_heads
 
-    def get_shows(self):
+    def __get_shows(self):
         return self.__shows
 
-    def get_show(self):
+    def __get_show(self):
         return self.__show
 
-    terms = property(get_terms)
-    subdoms = property(get_subdoms)
-    facts = property(get_facts)
-    ng_heads = property(get_ng_heads)
-    shows = property(get_shows)
-    show = property(get_show)
+    terms = property(__get_terms)
+    subdoms = property(__get_subdoms)
+    facts = property(__get_facts)
+    ng_heads = property(__get_ng_heads)
+    shows = property(__get_shows)
+    show = property(__get_show)
 
 
